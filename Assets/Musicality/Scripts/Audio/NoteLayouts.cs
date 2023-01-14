592927540aea08410e252f9007b0f4968c60b008
MarshallChadbourne <mchadbourne@berklee.edu>
2023-01-14 17:22:38 -0500

    Wrote method for determining which pitch a grabbed object

--- Assets/Musicality/Scripts/Audio/NoteLayouts.cs
+++ Assets/Musicality/Scripts/Audio/NoteLayouts.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 namespace Foundry
 {
     //"Eb2", "E2", "F2", "Gb2", "G2", "Ab2", "A2", "Bb2", "B2", "C3", "Db3", "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3", "B3";
-    public static class MelodyLayouts
+    public static class NoteLayouts
     {
         public static string[] APentatonic = {"A2", "B2", "Db3", "E3", "Gb3"};
-
         
     }
 }
