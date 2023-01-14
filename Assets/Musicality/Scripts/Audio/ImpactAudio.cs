ec6a71414981b49d4a2cc7b88bc98000c1687cac
MarshallChadbourne <mchadbourne@berklee.edu>
2023-01-14 10:20:44 -0500

    Completed impact audio assemblies

--- Assets/Musicality/Scripts/Audio/ImpactAudio.cs
+++ Assets/Musicality/Scripts/Audio/ImpactAudio.cs
@@ -7,8 +7,10 @@ namespace Foundry
     public class ImpactAudio : MonoBehaviour
     {
         public List<AudioClip> hardImpactClips;
+        public List<AudioClip> softImpactClips;
 
         public Dictionary<string, AudioClip> hardImpactsDictionary = new Dictionary<string, AudioClip>();
+        public Dictionary<string, AudioClip> softImpactsDictionary = new Dictionary<string, AudioClip>();
 
         void Awake()
         {
@@ -17,12 +19,7 @@ namespace Foundry
 
         void Start()
         {   
-            // Add each audio clip into the dictionary. These can then be accessed by it's corresponding musical note, rather than it's index.
-            string[] notes = { "Eb2", "E2", "F2", "Gb2", "G2", "Ab2", "A2", "Bb2", "B2", "C3", "Db3", "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3" };
-            for (int i = 0; i < hardImpactClips.Count; i++)
-            {
-                hardImpactsDictionary.Add(notes[i], hardImpactClips[i]);
-            }
+            LoadClipDictionarys();
         }
 
         public void PlayImpactClip(string clipName, AudioSource objectSource)
@@ -36,6 +33,18 @@ namespace Foundry
             else
             {
                 Debug.LogError("Clip not found: " + clipName);
+            }
+        }
+
+
+        private void LoadClipDictionarys()
+        {
+            // Add each audio clip into the dictionary. These can then be accessed by it's corresponding musical note, rather than it's index.
+            string[] notes = { "Eb2", "E2", "F2", "Gb2", "G2", "Ab2", "A2", "Bb2", "B2", "C3", "Db3", "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3" };
+            for (int i = 0; i < hardImpactClips.Count; i++)
+            {
+                hardImpactsDictionary.Add(notes[i], hardImpactClips[i]);
+                softImpactsDictionary.Add(notes[i], hardImpactClips[i]);
             }
         }
     }
