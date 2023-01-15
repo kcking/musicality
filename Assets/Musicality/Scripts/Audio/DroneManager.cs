61c6e4d7077311cd4de6b575391b148d3bb869f2
MarshallChadbourne <mchadbourne@berklee.edu>
2023-01-14 19:55:15 -0500

    Added Drones and mesh collider to hands

--- Assets/Musicality/Scripts/Audio/DroneManager.cs
+++ Assets/Musicality/Scripts/Audio/DroneManager.cs
@@ -6,16 +6,41 @@ namespace Foundry
 {
     public class DroneManager : MonoBehaviour
     {
-        // Start is called before the first frame update
+        public List<AudioClip> droneClips;
+        public List<string> droneRoots;
+        public Dictionary<string, AudioClip> droneDictionary;
+
+        private GameManager gameManager;
+
         void Start()
         {
+            FindObjectOfType<GameManager>();
+            StoreClipsInDict();
+        }
         
+        ///<summary>
+        /// Plays the drone clip of the given root note
+        ///</summary>
+        public void PlayDroneClip(string droneRoot, AudioSource audioSource)
+        {
+            if(droneDictionary.ContainsKey(droneRoot))
+            {
+                audioSource.clip = droneDictionary[droneRoot];
+                audioSource.Play();
+            } else 
+            {
+                Debug.Log("Drone root not found!");
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        private void StoreClipsInDict()
         {
-        
+            int index = 0;
+            foreach (AudioClip drone in droneClips)
+            {
+                droneDictionary.Add(droneRoots[index], droneClips[index]);
+            }
         }
+
     }
 }
