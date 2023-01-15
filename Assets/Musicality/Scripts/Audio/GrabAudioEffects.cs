277f13253d3a64e891b133db6beab0074a45a47d
Kevin King <kcking@users.noreply.github.com>
2023-01-15 03:04:19 -0500

    color feedback on grabbed notes

--- Assets/Musicality/Scripts/Audio/GrabAudioEffects.cs
+++ Assets/Musicality/Scripts/Audio/GrabAudioEffects.cs
@@ -10,11 +10,13 @@ namespace Foundry
         public SoundObject soundObject;
         // The current note value that the object would trigger if struck
         public string currentNoteValue = null;
+        public Color? currentColor = null;
 
         public bool isGrabbed;
 
         // What note values will be held by the differnet positions
         public string[] positionNotes = NoteLayouts.APentatonic;
+        static Color[] colors = { new Color(1.0f, 0.0f, 0.0f), new Color(1.0f, 127.0f / 255.0f, 0.0f), new Color(1.0f, 1.0f, 0.0f), new Color(0.0f, 1.0f, 0.0f), new Color(0.0f, 0.0f, 1.0f) };
 
         [SerializeField]
         private static float numberOfSections = 5.0f;
@@ -31,6 +33,14 @@ namespace Foundry
         void Update()
         {
             // Debug.Log("UpdateKevin");
+            if (isGrabbed)
+            {
+                UpdateCurrentNote();
+                if (currentColor != null)
+                {
+                    GetComponent<MeshRenderer>().material.color = (Color)currentColor;
+                }
+            }
         }
 
         void OnCollisionEnter(Collision collision)
@@ -73,14 +83,18 @@ namespace Foundry
         // Updates the current note value depending on the postion of the grabbed object. 
         private void UpdateCurrentNote()
         {
-            float yPos = gameObject.transform.position.y;
+            float yPos = gameObject.transform.position.y - 0.2f; // translate down a little
             int section = (int)(yPos / sectionSize);
 
             if (yPos > 2.0)
-                section = positionNotes.Length;
+                section = positionNotes.Length - 1;
             if (yPos < 0.0)
                 section = 0;
 
+            if (section < colors.Length)
+            {
+                currentColor = (Color)colors[section];
+            }
             currentNoteValue = positionNotes[section];
         }
 
